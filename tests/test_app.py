dfde46b5c96237acb197635638c5648bb759a890
Anurag stark <Anuragchauhan563@gmail.com>
2025-04-12 12:46:00 +0530

    fixed test_app file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
 import pytest
+# from app.app import app as flask_app
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.app import app as flask_app
+
 
 @pytest.fixture
 def app():
